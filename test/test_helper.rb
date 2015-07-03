f85417ee6ca34fb97a0228a37fb60d9900e7dce3
Mike Kenyon <mike.kenyon@gmail.com>
2015-07-03 15:22:55 -0400

    Prefer SpecReporter to DefaultReporter

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -4,5 +4,5 @@ require 'video_url_parser'
 
 reporter_options = { :color => true }
 Minitest::Reporters.use! [
-  Minitest::Reporters::DefaultReporter.new(reporter_options)
+  Minitest::Reporters::SpecReporter.new(reporter_options)
 ]
